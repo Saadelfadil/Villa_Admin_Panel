0e90720f65792e21a85d59e6ab560ad7d53ed68f
saadelfadil <saadelfadil18@gmail.com>
2022-01-16 15:19:04 +0100

    Add, delete and Edit Villa done (still fix frontend)

--- php_files/edit_villa.php
+++ php_files/edit_villa.php
@@ -12,15 +12,23 @@ if (isset($_GET['id']) AND !empty($_GET['id']))
 		$villa_city = str_replace('<br />', '', $villaInfo['villa_city']);
 		$villa_price = $villaInfo['villa_price'];
         $villa_days = $villaInfo['villa_days'];
+		$villa_picture = $villaInfo['villa_picture'];
 		
 		if (isset($_POST['EditVilla']))
 		{
+			
 			$newVilla_name = htmlspecialchars($_POST['villa_name']);
 			$newVilla_city = nl2br(htmlspecialchars($_POST['villa_city']));
 			$newVilla_price = htmlspecialchars($_POST['villa_price']);
         	$newVilla_days = htmlspecialchars($_POST['villa_days']);
-			$updateVilla = $bdd->prepare('UPDATE villa SET villa_name = ?, villa_city = ?, villa_price = ?, villa_days = ? WHERE id= ?');
-			$updateVilla->execute(array($newVilla_name, $newVilla_city, $newVilla_price, $newVilla_days, $getId));
+			$newVilla_pic = time() . '_' . $_FILES['villa_picture']['name'];
+			$newVilla_picture = '../assets/img/' . $newVilla_pic;
+			if (empty($_FILES['villa_picture']))
+				$newVilla_picture = $villaInfo['villa_picture'];
+			else
+				move_uploaded_file($_FILES['villa_picture']['tmp_name'], $newVilla_picture);
+			$updateVilla = $bdd->prepare('UPDATE villa SET villa_picture = ?, villa_name = ?, villa_city = ?, villa_price = ?, villa_days = ? WHERE id= ?');
+			$updateVilla->execute(array($newVilla_picture, $newVilla_name, $newVilla_city, $newVilla_price, $newVilla_days, $getId));
 			header('Location: villa.php');
 		}
 	}
@@ -32,27 +40,57 @@ else
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-    <title> Edit Villa</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../assets/css/style_1.css">
-    </head>
-    <body>
-
-	<form method="POST" action="">
-        <div class="login-box">
-            <h1>Edit Villa</h1>
-            <div class="textbox">
-                <input type="text" placeholder="Villa Name" name="villa_name" value="">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/toastr.scss">
+    <title>Login page</title>
+</head>
+<body class="container">
+    <header>
+        <form id="form1" method="POST" action="" type="hidden">
+        </form>
+            <button type="submit" form="form1" name="back" >
+                <img src="../assets/img/arrow.svg" alt="">
+            </button>
+            <h1>Ürün Ekleme</h1>
+    </header>
+    <form action="" method="post"  enctype="multipart/form-data">
+        <div class="image">
+            <div class="images_upload">
+                <label for="villa_picture"><img src="<?= $villa_picture?>"></label>
+                <input type="file" name="villa_picture" id="villa_picture" />
             </div>
-
-            <div class="textbox">
-                <input type="text" placeholder="Villa City" name="villa_city" value="">
+            <div class="title">
+                <h2>Listelemek istediğinoz eşyanin fotoğrafini ekleyin</h2>
             </div>
-
-            <input type="submit" class="btn" name="EditVilla" value="Edit">
         </div>
-      </form>
-    </body>
+        <div class="formation">
+            <h4>Hiç resim seçilmedi</h4>
+            <input type="text" name="villa_name" placeholder="Eşya Adi*" value="<?= $villa_name?>">
+            <textarea name="villa_city" id="" cols="30" rows="10" placeholder="Eşya Açiklamasi*"><?= $villa_city?></textarea>
+            <input type="text" name="villa_price" placeholder="Eşyanizin Tahmini Fiyati*" value="<?= $villa_price?>">
+            <input type="text" name="villa_days" placeholder="Eşyanizin Gün Sayisi*" value="<?= $villa_days?>">
+            <div class="select">
+                <select name="" id="">
+                    <option value="">Giyim</option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div>
+                <label for="">Hüküm ve Koşullar<img src="../assets/img/resize.png"></label>
+                <input type="checkbox">
+            </div>
+            <div class="button">
+                <button type="submit" name="EditVilla">Onayla ve Yolla</button>
+            </div>
+        </div>
+    </form>
+</body>
 </html>
